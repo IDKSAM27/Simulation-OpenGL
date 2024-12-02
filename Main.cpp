51974a49e8e5f6d8d8203a08d18a093a5773840f
Sampreet Patil <sampreetpatil270@gmail.com>
2024-12-02 18:53:45 +0530

    Gradient colors added to the triangle, modified VAO and default.vert and default.frag

--- Main.cpp
+++ Main.cpp
@@ -3,19 +3,21 @@
 #include<GLFW/glfw3.h>
 
 #include"shaderClass.h"
-#include"VAO.h"
-#include"VBO.h"
-#include"EBO.h"
+#include"VAO.h"	// Vertex Array Object
+#include"VBO.h" // Vertex Buffer Object
+#include"EBO.h" // Element Buffer Object
 
 // Vertices coordinates
 GLfloat vertices[] =
 {
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+	// Basically I've added individua color to each vertices 
+	//			COORDINATES							COLORS
+	-0.5f,  -0.5f * float(sqrt(3)) / 3,		0.0f,  0.8f, 0.3f,  0.02f, // Lower left corner
+	 0.5f,  -0.5f * float(sqrt(3)) / 3,		0.0f,  0.8f, 0.3f,  0.02f, // Lower right corner
+	 0.0f,   0.5f * float(sqrt(3)) * 2 / 3, 0.0f,  1.0f, 0.6f,  0.32f, // Upper corner
+	-0.25f,  0.5f * float(sqrt(3)) / 6,		0.0f,  0.9f, 0.45f, 0.17f, // Inner left
+	 0.25f,  0.5f * float(sqrt(3)) / 6,		0.0f,  0.9f, 0.45f, 0.17f, // Inner right
+	 0.0f,  -0.5f * float(sqrt(3)) / 3,		0.0f,  0.8f, 0.3f,  0.02f  // Inner down
 };
 
 // Indices for vertices order
@@ -41,7 +43,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 800, "SimulationOpenGL", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
@@ -73,7 +75,8 @@ int main()
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO to VAO
-	VAO1.LinkVBO(VBO1, 0);
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
@@ -113,4 +116,6 @@ int main()
 	glfwTerminate();
 	return 0;
 }
+
+
 
